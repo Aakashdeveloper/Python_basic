20edc95f1f238e4c06f1ad1736f53417f37cddc2
Avyaan <avi@Avyaans-MacBook-Pro.local>
2018-08-02 00:14:45 +0530

    a

---
+++ first.py
@@ -0,0 +1,14 @@
+marks = 67
+subject = "Python"
+status = False
+
+'Python Angular'  ==  "Python Angular"  ==  """Python Angular"""
+
+name = "Charan"
+name.upper
+
+name = "Charan"
+subject = "Python"
+
+"my name is charan. I am learning python"
+"My name is {0}. I am learning {1}".format(name, subject)
---
+++ second.py
@@ -0,0 +1,137 @@
+>>> marks = 80
+>>> marks
+80
+>>> subject = "python"
+>>> subject
+'python'
+>>> marks + subject
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+>>> subject + marks
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: must be str, not int
+>>> true + 1
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'true' is not defined
+>>> True + 1
+2
+>>> "pythob"+ 1
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: must be str, not int
+>>> True + 1
+2
+>>> False 
+False
+>>> False + 2
+2
+>>> avg = 10.890
+>>> marks = 10
+>>> avg + marks
+20.89
+>>> int(avg) == 10
+True
+>>> float(marks) = 10.00
+  File "<stdin>", line 1
+SyntaxError: can't assign to function call
+>>> float(marks) == 10.00
+True
+>>> float(marks) == 10.10
+False
+>>> firstName = "John"
+>>> lastName= "mehew"
+>>> firstName+lastName
+'Johnmehew'
+>>> firstName = "John"
+
+
+marks = 80
+>>> marks
+80
+>>> subject = "python"
+>>> subject
+'python'
+>>> marks + subject
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: unsupported operand type(s) for +: 'int' and 'str'
+>>> subject + marks
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: must be str, not int
+>>> true + 1
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'true' is not defined
+>>> True + 1
+2
+>>> "pythob"+ 1
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: must be str, not int
+>>> True + 1
+2
+>>> False 
+False
+>>> False + 2
+2
+>>> avg = 10.890
+>>> marks = 10
+>>> avg + marks
+20.89
+>>> int(avg) == 10
+True
+>>> float(marks) = 10.00
+  File "<stdin>", line 1
+SyntaxError: can't assign to function call
+>>> float(marks) == 10.00
+True
+>>> float(marks) == 10.10
+False
+>>> firstName = "John"
+>>> lastName= "mehew"
+>>> firstName+lastName
+'Johnmehew'
+>>> firstName = "John"
+>>> clear
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'clear' is not defined
+>>> cls
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'cls' is not defined
+>>> exit()
+Avyaans-MacBook-Pro:~ avi$ clear
+
+Avyaans-MacBook-Pro:~ avi$ python3
+Python 3.6.5 (default, Apr 25 2018, 14:23:58) 
+[GCC 4.2.1 Compatible Apple LLVM 9.1.0 (clang-902.0.39.1)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> name = " Charan"
+>>> name.capitalize()
+' charan'
+>>> name = "Charan"
+>>> name.capitalize()
+'Charan'
+>>> name = "charan"
+>>> name.capitalize()
+'Charan'
+>>> name.upper()
+'CHARAN'
+>>> name.lower()
+'charan'
+>>> name.replace('n', 'nn')
+'charann'
+>>> name.isalpha()
+True
+>>> name.isdigit()
+False
+>>> mytext = "hi, i am doing, python"
+>>> mytext.split(",")
+['hi', ' i am doing', ' python']
+>>> 
+
