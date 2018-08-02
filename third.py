a83cf810c10c203a6b8774acac98a66dbad1b4c9
Avyaan <avi@Avyaans-MacBook-Pro.local>
2018-08-03 00:21:57 +0530

    a

---
+++ third.py
@@ -0,0 +1,72 @@
+python_course = True
+Angular_Course = False
+
+python_cousre = "True"
+
+'''null'''
+None
+
+london_population = None
+
+
+#If Else
+number = 6
+if number == 5:
+    print("number is 5")
+elif number == 6:
+    print("number is 6")
+else:
+    print("number is not 5")
+
+city = "Newyork"
+
+if city:
+    print("city exist")
+
+if not city:
+    print("no city")
+
+number = 3
+python_course = True
+
+if number == 3 and java_course:
+    print("this is i want")
+
+if number == 3 or python_course:
+    print("this i dont want")
+
+and == &&
+or == ||
+
+##### Ternary 
+
+a = 1
+b = 2 
+
+"bigger" if a>b else "smaller"
+
+if a>b:
+    print("bigger")
+else:
+    print("smaller")
+
+
+#a>b ? "bigger":"smaller" in javascript
+
+list
+Dictionries
+execption handling
+loops
+break & continue
+while Loop
+other data  types
+
+
+#Functions
+
+#OOPs in python class based
+
+#Small App
+
+#Django
+
